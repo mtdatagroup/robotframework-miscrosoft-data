b237339647ee590784690df21529ac1bb7346c38
Mark Stella <Mark.Stella@gmail.com>
2019-12-06 13:54:22 +1100

    Working inside of docker now (use WSL or Linux)

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
-FROM centos:7
+FROM centos:7 AS BASE
 
 LABEL description="Data-X Test Framework"
 LABEL maintainer="Mark Stella, Takuya Truong"
 LABEL version="1.0.0"
-
-WORKDIR /usr/src/app
 
 RUN curl https://packages.microsoft.com/config/rhel/7/prod.repo > /etc/yum.repos.d/mssql-release.repo
 #RUN yum remove -y unixODBC-utf16 unixODBC-utf16-devel
@@ -14,19 +12,25 @@ RUN yum install -y https://centos7.iuscommunity.org/ius-release.rpm && \
     yum -y install yum-utils wget && \
     yum -y install gcc-c++ python36u python36u-pip python36u-devel
 
-VOLUME /usr/src/features
+FROM BASE AS TF
+
+WORKDIR /usr/src/app
+VOLUME /usr/src/external
 
 ENV APP_ROOT /usr/src/app
 ENV LIB_DIR /usr/src/app/lib
-ENV FEATURES_DIR /usr/src/features
-ENV PATH $PATH:/opt/mssql-tools/bin
+
+#ENV PATH $PATH:/opt/mssql-tools/bin
 
 COPY requirements.txt /usr/src/app
-COPY lib /usr/src/app/lib
-
 RUN pip3.6 install -r requirements.txt
 
 ENV PYTHONPATH ${PYTHONPATH}:${LIB_DIR}
+
+COPY config /usr/src/app/config
+COPY lib /usr/src/app/lib
+COPY .env /usr/src/app
+COPY bootstrap.py /usr/src/app
 
 ENTRYPOINT ["/usr/bin/python3.6", "/usr/src/app/bootstrap.py"]
 
---
+++ run_me.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+PROGRAM_NAME=${0}
+LOCAL_DIR=${PWD}
+
+# Change below to update Tags and Container name
+IMAGE_NAME=data-x-tf-image
+IMAGE_TAG=$(cat VERSION)
+IMAGE_NAME_AND_TAG=${IMAGE_NAME}:${IMAGE_TAG}
+CONTAINER_NAME=data-x-tf-container
+
+NO_CACHE_DOCKER_BUILD_OPTS="--rm --no-cache"
+DOCKER_FILE=Dockerfile
+
+FINAL_IMAGE_PATH=${IMAGE_NAME}:${IMAGE_TAG}
+
+function usage()
+{
+   echo "${PROGRAM_NAME} [ -p | -b | -d | -a | -u | -8 | -c | -g | -r | -f ]"
+   echo " -b = build docker image"
+   echo " -c = build docker image - use no-cache"
+   echo " -i = interactive bash shell"
+   echo " -p = docker system prune"
+   echo " -t = run the container"
+   exit 1
+}
+
+function docker_system_prune()
+{
+   docker system prune
+}
+
+function build_image()
+{
+   echo "Building image ..."
+   docker build -f ${DOCKER_FILE} -t ${IMAGE_NAME_AND_TAG} .
+}
+
+function build_image_no_cache()
+{
+   echo "Building image ..."
+   docker build ${NO_CACHE_DOCKER_BUILD_OPTS} -f ${DOCKER_FILE} -t ${IMAGE_NAME_AND_TAG} .
+}
+
+function run_interactive()
+{
+   echo "Running interactive session ..."
+
+   docker run --rm -it \
+      --entrypoint "/bin/bash" \
+      --name ${CONTAINER_NAME}-INTERACTIVE \
+      -v ${LOCAL_DIR}/external:/usr/src/external \
+      -v ${LOCAL_DIR}/lib:/usr/src/app/lib \
+      -v ${LOCAL_DIR}/tests:/usr/src/app/tests \
+      ${FINAL_IMAGE_PATH} 
+}
+
+function run_container()
+{
+   echo "Running container ..."
+
+   docker run --rm -it \
+      --name ${CONTAINER_NAME}-INTERACTIVE \
+      -v ${LOCAL_DIR}/external:/usr/src/external \
+      -v ${LOCAL_DIR}/lib:/usr/src/app/lib \
+      -v ${LOCAL_DIR}/tests:/usr/src/app/tests \
+      ${FINAL_IMAGE_PATH} 
+}
+
+###############
+#    main()   #
+###############
+
+build=0
+build_no_cache=0
+interactive=0
+container=0
+prune=0
+
+while getopts "bicpt" opt; do
+  case ${opt} in
+   i )
+      interactive=1
+      ;;
+   \? )
+      usage
+      ;;
+   b )
+      build=1
+      ;;
+   c )
+      build_no_cache=1
+      ;;
+   p )
+      prune=1
+      ;;
+   t )
+      container=1
+      ;;
+  esac
+done
+
+if [[ ${build} -eq 1 ]] ; then
+   build_image
+fi
+
+if [[ ${build_no_cache} -eq 1 ]] ; then
+   build_image_no_cache
+fi
+
+if [[ ${interactive} -eq 1 ]] ; then
+   run_interactive
+fi
+
+if [[ ${container} -eq 1 ]] ; then
+   run_container
+fi
+
+if [[ ${prune} -eq 1 ]] ; then
+   docker_system_prune
+fi
